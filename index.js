f000bd12b8d3e050f68381c732d914de005d2734
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-05-19 20:50:41 +0600

    api loaded

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const res = require('express/lib/response');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const res = require("express/lib/response");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -11,22 +11,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6w3c6.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-   console.log('Book House Connected')
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const dataCollection = client.db("Books").collection("data");
 
-app.get('/', (req, res) => {
-   res.send('Running Book Server');
+    app.get("/data", async (req, res) => {
+      const query = {};
+      const cursor = dataCollection.find(query);
+      const data = await cursor.toArray();
+      res.send(data);
+    });
+
+
+    app.get('/data/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query ={_id: ObjectId(id)};
+      const data = await dataCollection.findOne(query);
+      res.send(data);
+    })
+
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Book Server");
 });
 
-app.listen(port, () =>{
-  console.log('Listening to port', port);
-})
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
