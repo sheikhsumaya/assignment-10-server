070bd6f91230369cc95d9f10420895f9ace30716
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-05-19 19:53:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const res = require('express/lib/response');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6w3c6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+   console.log('Book House Connected')
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/', (req, res) => {
+   res.send('Running Book Server');
+});
+
+app.listen(port, () =>{
+  console.log('Listening to port', port);
+})
