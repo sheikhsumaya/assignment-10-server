e1b521388715e4ea3e8ee8aadc20be493d380312
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-05-21 00:47:02 +0600

    cors

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6w3c6.mongodb.net/?retryWrites=true&w=majority`;
